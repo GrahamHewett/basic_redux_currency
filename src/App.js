40965be17d41bec48b522fafee6bdfe814dc0437
Graham Hewett <hewettgraham@gmail.com>
2020-02-09 21:47:55 +0000

    comment out logRocket for now

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import LogRocket from 'logrocket';
-import debounce from "lodash.debounce";
-import axios from "axios";
 import CurrencyConversion from "./components/currencyconversion";
 
-LogRocket.init('f0e3q8/redux-test');
-LogRocket.identify('hewettgraham@gmail.com');
+// import debounce from "lodash.debounce";
+// import axios from "axios";
+// import LogRocket from 'logrocket';
+// LogRocket.init('f0e3q8/redux-test');
+// LogRocket.identify('hewettgraham@gmail.com');
 
 export default function App() {
 	return (
