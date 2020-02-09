ee69278eb659100b26ffc336d56979a4b8b4f26e
Graham Hewett <hewettgraham@gmail.com>
2020-02-09 21:49:02 +0000

    rm console logs + dispatch handler for base amount

--- src/components/currencyconversion.js
+++ src/components/currencyconversion.js
@@ -2,19 +2,22 @@ import React from 'react'
 import { connect } from "react-redux";
 
 function Currencyconversion(props) {
-  console.log("props", props)
+
+  const setBaseAmount = data => {
+    return props.dispatch({ type: "CHANGE_BASE_AMOUNT", data})
+  }
+
   return (
     <>
       <h1>BaseAmount = {props.baseAmount}</h1>
       <h1>Counter = {props.counter}</h1>
       <button onClick={() => props.dispatch({ type: 'INCREMENT' })}>INCREMENT</button>
       <button onClick={() => props.dispatch({type: "DECREMENT"})}>DECREMENT</button>
+      <input type="number" placeholder="change base amount" onChange={(e) => setBaseAmount(e.target.value)}></input>
     </>
   )
 }
 
 export default connect((state, props) => {
-  console.log("connect state ", state)
-  console.log("connect props ", props)
   return {baseAmount: state.baseAmount, counter: state.counter}
 })(Currencyconversion)
